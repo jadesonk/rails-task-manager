8c5220f4c73630d0aba5de98f40321a6b02cb8ae
jadeson komolvipat <jadesonk@gmail.com>
2020-09-08 14:33:04 +0700

    create page and feature to add a new task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,24 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    # take all the data from params and create a new restaurant
+    @task = Task.new(task_params) # taking in params
+    # save it to the database
+    @task.save
+    # redirect the user
+    redirect_to tasks_path # keyword for rails 'redirect_to'
+    # redirect_to restaurant_path(@resto)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
 end
